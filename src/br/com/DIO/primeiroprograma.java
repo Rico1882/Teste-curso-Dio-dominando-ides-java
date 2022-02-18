363185d73b2f5a315a526029cab61fd7ecaec108
Rico1882 <rico_1882@hotmail.com>
2022-02-18 17:40:49 -0300

    First Commit

---
+++ src/br/com/DIO/primeiroprograma.java
@@ -0,0 +1,21 @@
+package br.com.DIO;
+
+import br.com.DIO.model.Gato;
+
+public class primeiroprograma {
+
+	public static void main(String[] args) {
+		
+		Gato gato = new Gato();
+		
+		System.out.println(gato);	
+		
+		
+	/*int a = 2;
+		int b = 3;
+		System.out.println("Hello World " + (a+b));*/
+
+	}
+
+		
+}
